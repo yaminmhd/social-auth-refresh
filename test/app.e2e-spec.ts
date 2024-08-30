af10e06c98eed46622e112e282d05a3904e015a4
Yamin <mdyamin.drzee@gmail.com>
2024-08-30 12:13:06 +0800

    add e2e test for jwt auth local strategy flow

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -40,4 +40,33 @@ describe('AppController (e2e)', () => {
       .send(createUserDto)
       .expect(HttpStatus.CREATED);
   });
+
+  it('/auth/login (POST)', () => {
+    const createUserDto: CreateUserRequest = {
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    return request(app.getHttpServer())
+      .post('/users')
+      .send(createUserDto)
+      .expect(HttpStatus.CREATED)
+      .then(() => {
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send(createUserDto)
+          .expect(HttpStatus.CREATED)
+          .expect('Set-Cookie', /Authentication=.+/)
+          .then((response) => {
+            const cookies = response.headers['set-cookie'];
+            expect(cookies).toBeDefined();
+
+            const cookieDescriptors = cookies[0].split(';');
+            expect(cookieDescriptors[0]).toContain('Authentication');
+            expect(cookieDescriptors[1]).toContain('Path');
+            expect(cookieDescriptors[2]).toContain('Expires');
+            expect(cookieDescriptors[3]).toContain('HttpOnly');
+            expect(cookieDescriptors.length).toEqual(4);
+          });
+      });
+  });
 });
